51f74af9c282613c5caa2cb0ddaf7c60ca707547
Marius <munteanymarius@gmail.com>
2023-07-23 17:07:29 +0300

    for zayka

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Wrapper } from './components/TodoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <Wrapper />
+    </div>
+  );
+}
+
+export default App;
